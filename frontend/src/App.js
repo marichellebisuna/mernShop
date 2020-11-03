70d89ef5c0834a9cd333f7c4185c8b15d70f7dae
Marichelle Bisuna <marichellebisuna@yahoo.com>
2020-11-04 06:28:58 +1100

    Detailed Product created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Homescreen from './screens/HomeScreen';
+import Productscreen from './screens/ProductScreen';
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <Homescreen />
+          <Route exact path='/' component={Homescreen} />
+          <Route path='/product/:id' component={Productscreen} />
         </Container>
       </main>
 
       <Footer />
-    </>
+    </Router>
   );
 };
 
